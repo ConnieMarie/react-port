8e026a8b242bfaca21445a390578d7b1d6c984e9
conniemarie <constance.marie.strahan@gmail.com>
2023-02-02 18:26:30 -0600

    added emailjs for contact form functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,64 +1,114 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-import { validateEmail } from '../../utils/helpers';
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-
-  const [errorMessage, setErrorMessage] = useState('');
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const { name, email, message } = formState;
+  const form = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!errorMessage) {
-      console.log('Submit Form', formState);
+      emailjs
+        .sendForm(
+          "service_ujg9ene",
+          "template_i2hexrg",
+          form.current,
+          "x98v2CDYHOQyl7lzI"
+        )
+        .then(
+          (result) => {
+            setSuccessMessage("Your message has been sent successfully!");
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    } else {
+      console.log("Error:", errorMessage);
     }
+  
   };
 
   const handleChange = (e) => {
-    if (e.target.name === 'email') {
+    if (e.target.name === "email") {
       const isValid = validateEmail(e.target.value);
       if (!isValid) {
-        setErrorMessage('Your email is invalid.');
+        setErrorMessage("Your email is invalid.");
       } else {
-        setErrorMessage('');
+        setErrorMessage("");
       }
     } else {
       if (!e.target.value.length) {
         setErrorMessage(`${e.target.name} is required.`);
       } else {
-        setErrorMessage('');
+        setErrorMessage("");
       }
     }
     if (!errorMessage) {
       setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log('Handle Form', formState);
+      console.log("Handle Form", formState);
     }
   };
 
   return (
-    <section className='form-container'>
-      <h1 className='pageTitle'>Contact me</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
+    <section className="contact-me">
+      <h1 className="pageTitle">Contact me</h1>
+      <div className="form-container">
+      
+      <form id="contact-form" ref={form} onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
         </div>
         <div>
-          <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+          <textarea
+            name="message"
+            defaultValue={message}
+            rows="5"
+            onBlur={handleChange}
+          />
         </div>
         {errorMessage && (
           <div>
             <p className="error-text">{errorMessage}</p>
           </div>
         )}
-        <button type="submit">Submit</button>
+        {successMessage && (
+          <div>
+            <p className="success-text">{successMessage}</p>
+          </div>
+        )}
+        <button type="submit" onBlur={handleSubmit}>
+          Submit
+        </button>
       </form>
+      </div>
     </section>
   );
 }
